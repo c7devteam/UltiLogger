8693b57f4fb456ae9352f514b92e97e1d6e57bd9
Matthias Ludwig <matt.ludwig@me.com>
2016-03-15 16:19:21 +0100

    - created connection.js with connection-pool
    - installed mysql

--- routes/loggings_route.js
+++ routes/loggings_route.js
@@ -1,9 +1,6 @@
 "use strict";
-
+let connectionPool  =   require('../utils/connection');
 let routes = {};
-
-// server.post('/textlogging', loggings_route.createTextLogging);
-// server.post('/requestlogging', loggings_route.createRequestLogging);
 
 routes.createTextLogging = function(request, response, next) {
     console.log('create text logging');
