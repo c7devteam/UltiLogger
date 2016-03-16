112bce5c375b904ceeccbe7a6118a0eeb8097016
Matthias Ludwig <matt.ludwig@me.com>
2016-03-16 11:58:29 +0100

    - added applications_route.js
    - added implementation to add new application
    - moved connection to utils-folder

--- routes/loggings_route.js
+++ routes/loggings_route.js
@@ -2,11 +2,11 @@
 let connectionPool  =   require('../utils/connection');
 let routes = {};
 
-routes.createTextLogging = function(request, response, next) {
+routes.create_text_logging = function(request, response, next) {
     console.log('create text logging');
 };
 
-routes.createRequestLogging = function(request, response, next) {
+routes.create_request_logging = function(request, response, next) {
     console.log('create request logging');
 };
 
